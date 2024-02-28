6ace35fc68bced97dbcd6910185af225966b4a2a
Muhammad Naufal Adli <naufaladli0406@gmail.com>
2024-02-28 10:43:14 +0700

    Initial Commit

---
+++ NaufalCourseUI/NaufalCourseUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaufalCourseUIApp.swift
+//  NaufalCourseUI
+//
+//  Created by Naufal Adli on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct NaufalCourseUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
